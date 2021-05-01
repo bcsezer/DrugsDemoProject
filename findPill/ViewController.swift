42dc5e9c0e7e2c151d7100da3545fe1d0998d9f0
cem sezeroglu <cemsezeroglu@gmail.com>
2021-05-01 19:19:47 +0300

    Initial Commit

---
+++ findPill/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  findPill
+//
+//  Created by Cem on 1.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
